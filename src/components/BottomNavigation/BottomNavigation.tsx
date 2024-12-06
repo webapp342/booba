bb21ffe4f475470efc7125a4b932ea5c0a6aed19
19984646 <busetaktas45@gmail.com>
2024-12-06 23:23:23 +0330

    updated

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -85,6 +85,7 @@ export default function NavigationBar() {
           },
           "& .Mui-selected": {
             fontWeight: "bold",
+            color: "black",
           },
           transition: "none",
         }}
