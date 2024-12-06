61572dfddc3e963edf710949486176f4f3b46b31
19984646 <busetaktas45@gmail.com>
2024-12-06 13:35:58 +0330

    updated

--- src/app/bottom-navigation/page.tsx
+++ src/app/bottom-navigation/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Navigation from "@/components/BottomNavigation/BottomNavigation";
 
 
-export const HomePage: React.FC = () => {
+export default function Nav() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-end">
      <h2>merhaba </h2>
