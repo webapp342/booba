ec4ac9b489ed7a2566891ddbdc1f9a8cfb82a61f
19984646 <busetaktas45@gmail.com>
2024-12-07 00:34:23 +0330

    updated

--- src/app/bottom-navigation/page.tsx
+++ src/app/bottom-navigation/page.tsx
@@ -6,12 +6,9 @@ import Navigation from "@/components/BottomNavigation/BottomNavigation";
 
 export default function Nav() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-end">
-     <h2>merhaba </h2>
-      <div className="mb-6">
+  
         <Navigation />
-      </div>
-    </div>
+    
   );
 };
 
