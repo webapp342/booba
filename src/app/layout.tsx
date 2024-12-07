b73faaa20bc390ea411b979c565ce605ad74e149
19984646 <busetaktas45@gmail.com>
2024-12-07 07:28:17 +0330

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import type { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
 import { getLocale } from 'next-intl/server';
@@ -17,18 +18,27 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: PropsWithChildren) {
   const locale = await getLocale();
 
+  useEffect(() => {
+    // Safe area değişkenini sıfırlayın
+    const root = document.documentElement;
+    root.style.setProperty('--tgui--safe_area_inset_bottom', '0px');
+
+    // İlgili sınıfları hedefleyin ve padding'i sıfırlayın
+    const elements = document.querySelectorAll(
+      '.tgui-5dc6ad1ca3ac3ed4, .tgui-01790b7e59088ea5'
+    );
+    elements.forEach((el) => {
+      (el as HTMLElement).style.paddingBottom = '0px';
+    });
+  }, []);
+
   return (
     <html lang={locale}>
-    <body>
-      <I18nProvider>
-        <Root>
-          {children}
-        </Root>
-        
-      
-
-      </I18nProvider>
-    </body>
+      <body>
+        <I18nProvider>
+          <Root>{children}</Root>
+        </I18nProvider>
+      </body>
     </html>
   );
 }
