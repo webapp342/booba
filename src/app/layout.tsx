6e00490f67b4f33cb6fa03ac0a8ec7232e234680
19984646 <busetaktas45@gmail.com>
2024-12-07 07:29:56 +0330

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-
-import { useEffect } from 'react';
 import type { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
 import { getLocale } from 'next-intl/server';
@@ -9,7 +5,6 @@ import { getLocale } from 'next-intl/server';
 import { Root } from '@/components/Root/Root';
 import { I18nProvider } from '@/core/i18n/provider';
 
-import '@telegram-apps/telegram-ui/dist/styles.css';
 import 'normalize.css/normalize.css';
 import './_assets/globals.css';
 
@@ -21,27 +16,18 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: PropsWithChildren) {
   const locale = await getLocale();
 
-  useEffect(() => {
-    // Safe area değişkenini sıfırlayın
-    const root = document.documentElement;
-    root.style.setProperty('--tgui--safe_area_inset_bottom', '0px');
-
-    // İlgili sınıfları hedefleyin ve padding'i sıfırlayın
-    const elements = document.querySelectorAll(
-      '.tgui-5dc6ad1ca3ac3ed4, .tgui-01790b7e59088ea5'
-    );
-    elements.forEach((el) => {
-      (el as HTMLElement).style.paddingBottom = '0px';
-    });
-  }, []);
-
   return (
     <html lang={locale}>
-      <body>
-        <I18nProvider>
-          <Root>{children}</Root>
-        </I18nProvider>
-      </body>
+    <body>
+      <I18nProvider>
+        <Root>
+          {children}
+        </Root>
+        
+      
+
+      </I18nProvider>
+    </body>
     </html>
   );
 }
